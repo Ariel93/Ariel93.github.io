d8efcec6271cd4dc0897eef22ad27cfe001b2b50
Ariel93 <arieljeferson973@gmail.com>
2025-04-25 20:15:09 +0000

    atividade de java

---
+++ atividadejava/Questao01.js
@@ -0,0 +1,16 @@
+let n1 = 50
+let n2 = 60
+let n3 = 80
+let n4 = 50
+let  media = (n1 + n2+ n3+ n4) / 4;
+
+
+if (media >= 60) {
+    console.log("esta aprovado")
+}
+else if (media >= 40 && media < 60) {
+    console.log("o aluno esta em recuperacao")
+} else {
+    console.log("o aluno esta reprovado")
+}
+
