092784a8177b5e2a4ff76fa0f79944d7bad6fc58
Ariel93 <arieljeferson973@gmail.com>
2025-05-09 11:45:16 +0000

    atividade de revisao

---
+++ atividadejava/revisaodemaio9/questao.js
@@ -0,0 +1,7 @@
+let idade = 18;
+let isadulto = (idade >= 18) ? 'sim' : 'não';
+
+// a crase é quando utiliza-se interpolacão
+console.log(`voce tem ${idade}.`);
+
+console.log('E adulto?' + isadulto);
---
+++ atividadejava/revisaodemaio9/suite.js
@@ -0,0 +1,15 @@
+let opcao  = 'a';
+
+switch (key) {
+    case 'a':
+        console.log('Media simples');
+        break;
+
+        case 'b':
+            console.log('Media ponderada');
+            break;
+
+        default:
+            console.log('opcao invalida');
+            break;
+}
