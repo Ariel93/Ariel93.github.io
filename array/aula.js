886cd7d6ae3dfc4d8845db4a546171acfd7daed2
Ariel93 <arieljeferson973@gmail.com>
2025-07-11 11:33:03 +0000

    array

---
+++ array/aula.js
@@ -0,0 +1,24 @@
+
+let frutas = ["maçã", "kiwi", "morango", "abacaxi"];
+
+// console.log('tamanho do array: ' + frutas.length);
+// console.log('array original: ' + frutas);
+
+// frutas.push("uva");
+// console.log(frutas);
+
+// frutas.push("banana");
+// console.log(frutas);
+// console.log(frutas.length);
+frutas[2] = "caju";
+frutas[4] = "pera";
+frutas.push("seriguela");
+console.log(frutas.sort());
+
+frutas.pop();
+frutas.pop();
+frutas.pop();
+console.log(frutas);
+
+
+
---
+++ array/carros.js
@@ -0,0 +1,26 @@
+
+let carros = [];
+
+carros.push('corolla');
+carros.push('sw4');
+carros.push('onix');
+carros.push('fusca');
+
+carros [2] = "amarok";
+
+carros.pop();
+
+carros.push('l200')
+carros.push('a3')
+carros.push('uno')
+
+for(let i = 0; i < carros.length; i++){
+   console.log("indice: " + i);
+   console.log("valor: " + carros[i]);
+   console.log();
+   
+   
+   
+
+}
+
