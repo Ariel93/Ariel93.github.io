31055ed143b2b76e075242171859d037f0244fe0
Ariel93 <arieljeferson973@gmail.com>
2025-07-18 14:11:40 +0000

    atividade

---
+++ array/cores.js
@@ -0,0 +1,14 @@
+let cores = [];
+
+cores.push('verde')
+cores.push('amarelo')
+cores.push('azul')
+console.log(cores [1],cores [2]);
+cores.push('cinza')
+console.log(cores [3]);
+
+
+
+
+
+
---
+++ array/questao02.js
@@ -0,0 +1,4 @@
+let numeros = [5, 10, 15, 20, 25];
+
+console.log('5 esta na posição:',numeros.indexOf(5));
+
